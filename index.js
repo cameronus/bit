feccbf52e550631f4efc04676ade58114711fbab
Cameron Jones <cameroncjones4@gmail.com>
2016-04-12 15:24:31 -0700

    Initial server code

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var session = require('express-session');
+var shortid = require('shortid');
+var bodyParser = require("body-parser");
+var app = express();
+var router = express.Router();
+var port = process.env.PORT || 80;
+shortid.seed(1942);
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  genid: function(req) {
+    return shortid.generate();
+  },
+  secret: 'a8dm38dsnw02n5n7k10dj',
+  resave: false,
+  saveUninitialized: false
+}))
+
+
+router.get('/', function(req, res) {
+  var sess = req.session;
+  var hidden = shortid.generate();
+
+  sess.hidden = hidden;
+  res.render('pages/main', { hidden: hidden, error: "" });
+});
+
+router.post('/', function(req, res) {
+  var sess = req.session;
+  var hidden = req.body.hidden;
+
+  if (req.body.text == "") {
+    res.render('pages/error', { error: "Enter text into form." });
+    sweetAlert("Oops...", "Something went wrong!", "error");
+  } else if (hidden != sess.hidden) {
+    res.render('pages/error', { error: "Invalid form submission." });
+    sweetAlert("Oops...", "Something went wrong!", "error");
+  } else {
+    res.render('pages/success', { success: "success!" });
+  }
+});
+
+router.get('/hello/:name', function(req, res) {
+  res.send('hello ' + req.params.name + '!');
+});
+
+app.use('/', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
